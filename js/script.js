bea7b53e944a52ed92d3dc5a526c92ba7130243c
SrunTechsean <techseansrun@gmail.com>
2026-04-17 02:25:44 +0700

    feat: dynamically add icon into the logs

--- js/script.js
+++ js/script.js
@@ -10,6 +10,144 @@ const state = {
     entries: [],
 };
 
+const icon = {
+    breakfast: `<svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="26"
+                                    height="26"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="icon icon--breakfast"
+                                    aria-hidden="true"
+                                >
+                                    <circle cx="12" cy="12" r="4" />
+                                    <path d="M12 2v2" />
+                                    <path d="M12 20v2" />
+                                    <path d="m4.93 4.93 1.41 1.41" />
+                                    <path d="m17.66 17.66 1.41 1.41" />
+                                    <path d="M2 12h2" />
+                                    <path d="M20 12h2" />
+                                    <path d="m6.34 17.66-1.41 1.41" />
+                                    <path d="m19.07 4.93-1.41 1.41" />
+                                </svg>`,
+    lunch: `<svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="26"
+                                    height="26"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="icon icon--breakfast"
+                                    aria-hidden="true"
+                                >
+                                    <circle cx="12" cy="12" r="4" />
+                                    <path d="M12 2v2" />
+                                    <path d="M12 20v2" />
+                                    <path d="m4.93 4.93 1.41 1.41" />
+                                    <path d="m17.66 17.66 1.41 1.41" />
+                                    <path d="M2 12h2" />
+                                    <path d="M20 12h2" />
+                                    <path d="m6.34 17.66-1.41 1.41" />
+                                    <path d="m19.07 4.93-1.41 1.41" />
+                                </svg>`,
+    dinner: `<svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="26"
+                                height="26"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="icon icon--dinner"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"
+                                />
+                            </svg>`,
+    snack: `<svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="26"
+                                height="26"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="icon icon--snack"
+                                aria-hidden="true"
+                            >
+                                <path d="M12 6.528V3a1 1 0 0 1 1-1h0" />
+                                <path
+                                    d="M18.237 21A15 15 0 0 0 22 11a6 6 0 0 0-10-4.472A6 6 0 0 0 2 11a15.1 15.1 0 0 0 3.763 10 3 3 0 0 0 3.648.648 5.5 5.5 0 0 1 5.178 0A3 3 0 0 0 18.237 21"
+                                />
+                            </svg>`,
+    custom: `<svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="26"
+                                height="26"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="icon icon--custom"
+                                aria-hidden="true"
+                            >
+                                <circle cx="12" cy="12" r="10" />
+                                <path d="M8 12h8" />
+                                <path d="M12 8v8" />
+                            </svg>`,
+    edit: `<svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="icon edit__icon"
+                                >
+                                    <title>Edit</title>
+                                    <path
+                                        d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
+                                    />
+                                    <path d="m15 5 4 4" />
+                                </svg>`,
+    remove: `<svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    class="icon trash__icon"
+                                >
+                                    <title>Delete</title>
+                                    <path d="M10 11v6" />
+                                    <path d="M14 11v6" />
+                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                                    <path d="M3 6h18" />
+                                    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                                </svg>`,
+};
+
 loadEntries();
 
 function loadEntries() {
@@ -28,7 +166,9 @@ function addMeal(mealType, name, calories) {
     saveEntries();
 }
 
-addMeal("Breakfast", "Chicken", 200);
+if (state.entries.length === 0) {
+    addMeal("dinner", "Chicken", 200);
+}
 
 function saveEntries() {
     localStorage.setItem("SeanTrack-entries", JSON.stringify(state.entries));
@@ -52,6 +192,11 @@ function renderMeal() {
         const mealItem = document.createElement("li");
         mealItem.classList.add("meal__item");
 
+        // MealType Icon
+        const mealIcon = document.createElement("span");
+        mealIcon.innerHTML = icon[entry.mealType.toLowerCase()];
+
+        // Container for mealtype and mealname
         const mealInfo = document.createElement("div");
         mealInfo.classList.add("meal__info");
 
@@ -63,11 +208,19 @@ function renderMeal() {
         mealName.classList.add("meal__name");
         mealName.textContent = entry.name;
 
+        // Calorie of the Meal
         const mealCal = document.createElement("p");
         mealCal.classList.add("meal__macros");
         mealCal.textContent = `${entry.calories} kcal`;
 
-        mealItem.append(mealInfo, mealCal);
+        // Edit Icon and Remove Icon
+        const edit = document.createElement("span");
+        edit.innerHTML = icon.edit;
+
+        const remove = document.createElement("span");
+        remove.innerHTML = icon.remove;
+
+        mealItem.append(mealIcon, mealInfo, mealCal, edit, remove);
         mealInfo.append(mealType, mealName);
 
         // add that itme into the log
